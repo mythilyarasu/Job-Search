d3746f99820d143f7ceefdcd2682ce316c532ffe
mythilyarasu <mythilyarasu@gmail.com>
2023-08-10 15:12:24 +0530

    update

--- src/Components/JobDiv/Jobs.jsx
+++ src/Components/JobDiv/Jobs.jsx
@@ -109,7 +109,7 @@ const Jobs  = () => {
               <img src={image} alt="Company logo" className="w-[10%]" />
               <span className='text-[14px] py-[1rem] block group-hover:text-white'>{company}</span>
             </div>
-            <button className='border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-textColor hover:bg-white  group-hover/item:text-textColor group-hover:text-white  '>
+            <button className='border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-textColor hover:bg-white  group-hover/item:text-[#000000] group-hover:text-white  '>
               Apply Now
             </button>
   
